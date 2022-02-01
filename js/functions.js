89f955d5f8ebffd6f645083953f5150195156a23
Douglas Oliveira <contato@douglasoliveira.cc>
2022-02-01 15:40:17 -0300

    Melhorias no site

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,9 @@
 window.onload = function() {
   let buttonMenu = document.querySelector("#menu");
   buttonMenu.addEventListener("click", function(){
-    this.firstChild.classList.toggle("open");
     this.classList.toggle("menu-open");
   });
-
 }
 
-document.onkeydown = downSection;
 
-function downSection(e) {
-  if (e.key == 'ArrowDown') {
-    var elementHeight = document.querySelector("main").clientHeight;
-    window.scrollTo({
-      top: elementHeight,
-      behavior: 'smooth'
-    });
-  }
-}
+
