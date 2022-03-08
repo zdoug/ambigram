ea9d4010fe5cef7ed030a4d854ed5081fc957fba
Douglas Oliveira <contato@douglasoliveira.cc>
2022-03-08 20:00:12 -0300

    correção nos caminhos de pastas

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const plumber = require("gulp-plumber");
 const uglify  = require("gulp-uglify");
 const concat  = require("gulp-concat");
 const rename  = require("gulp-rename");
-const sass    = require('gulp-sass')(require('sass'));
+const sass = require('gulp-sass')(require('sass'));
 
 const js_src = "./src/js/*.js";
 const js_dist  = "./js/";
 const js_dist_name = "scripts.js";
 
 gulp.task('styles', function(){
-    return gulp.src("./scss/*.scss")
+    return gulp.src("./src/scss/*.scss")
       .pipe(sass.sync().on("error", sass.logError))
       .pipe(gulp.dest("./css"));
 });
@@ -26,7 +26,7 @@ gulp.task('scripts', function(){
 
 gulp.task('watch', function(){
     gulp.watch(js_src, gulp.series('scripts'));
-    gulp.watch('./scss/*.scss', gulp.series('styles'));
+    gulp.watch('./src/scss/*.scss', gulp.series('styles'));
 });
 
 gulp.task("default", gulp.series('watch'));
